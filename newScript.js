6081e93f8aaa74b330ef6761cc60c4871925d714
BbillahrariBBr <mb.billah8@gmail.com>
2022-08-15 10:48:54 +0600

    js dom 1 nd 2 problem solve

---
+++ newScript.js
@@ -0,0 +1,10 @@
+//all h5tags get elements by tag name
+const h5Tags = document.getElementsByTagName("h5");
+for (tag of h5Tags) {
+    tag.style.color = 'blue';
+    //console.log(h5Tag);
+}
+
+const backpackId = document.getElementById('backpack');
+backpackId.style.backgroundColor = 'tomato';
+// console.log(backpackId);
